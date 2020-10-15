13f5c6b6e283cc260045222429188624b99c30e9
Abenx <5195693+rushairer@users.noreply.github.com>
2020-10-16 00:40:19 +0800

    Initial Commit

---
+++ ABSwiftKitExtensionExample/ABSwiftKitExtensionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABSwiftKitExtensionExampleApp.swift
+//  ABSwiftKitExtensionExample
+//
+//  Created by Abenx on 2020/10/16.
+//
+
+import SwiftUI
+
+@main
+struct ABSwiftKitExtensionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABSwiftKitExtensionExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ABSwiftKitExtensionExample
+//
+//  Created by Abenx on 2020/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
