5c7123a85d86ed536f5234f4aae37025f664291d
Abenx <5195693+rushairer@users.noreply.github.com>
2020-11-05 22:44:15 +0800

    [Added]Add listStyleInsetGroupedListStyleSafety and appStoreOverlaySafety

--- Sources/ABSwiftKitExtension/View+ABSwiftKitExtension.swift
+++ Sources/ABSwiftKitExtension/View+ABSwiftKitExtension.swift
@@ -1,4 +1,7 @@
 import SwiftUI
+#if os(iOS)
+import StoreKit
+#endif
 
 public enum RoundedSize: Int, CaseIterable, Codable, Hashable, RawRepresentable  {
     case sm = 8
@@ -22,4 +25,31 @@ extension View {
             .clipShape(RoundedRectangle(cornerRadius: CGFloat(size.rawValue),
                                         style: .continuous))
     }
+    
+    #if os(iOS)
+    public func listStyleInsetGroupedListStyleSafety() -> some View {
+        Group {
+            if #available(iOS 14.0, *) {
+                self
+                    .listStyle(InsetGroupedListStyle())
+            } else {
+                self
+                    .listStyle(GroupedListStyle())
+            }
+        }
+    }
+    
+    public func appStoreOverlaySafety(isPresented: Binding<Bool>, appIdentifier: String) -> some View {
+        Group {
+            if #available(iOS 14.0, *) {
+                self.appStoreOverlay(isPresented: isPresented) {
+                    SKOverlay.AppConfiguration(appIdentifier: appIdentifier, position: .bottom)
+                }.self
+            } else {
+                self
+            }
+        }
+    }
+    #endif
+
 }
